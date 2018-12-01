062ab166d25894725686cbc6f0084a7360d6b1cf
hscanales <00136317@uca.edu.sv>
2018-12-01 17:39:38 -0600

    Finuche , Sick Love

---
+++ public/javascripts/index.js
@@ -0,0 +1,17 @@
+
+document.addEventListener("load",funcion());
+function funcion (){
+
+    var modal = document.getElementById("modal")
+
+    
+
+}
+
+function abrir() {
+    modal.style.display = "block";
+}
+
+function cerrar() {
+    modal.style.display = "none";
+}
